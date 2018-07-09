17b29feb1aef6022838262eeec2ac8d17d397832
guanjue <guanjuexiang@gmail.com>
2018-07-09 15:28:54 -0400

    tryp 10 cell types

---
+++ test_data/index_caller_pipeline.sh
@@ -0,0 +1,23 @@
+cut -f1,2 atac_20cell.sig.18.txt | awk -F '_' -v OFS='\t' '{print $1"_"$2"_"$3"_"$5"_"$6"_"$7"_"$8"_"$9"_"$10"_"$11"_"$15"_"$20}' > position_index.txt
+
+cut -f3,5,6,7,8,9,10,11,15,20 atac_20cell.sig.18.txt > signal_mat.txt
+
+
+paste position_index.txt signal_mat.txt | awk '{if ($2!="0_0_0_0_0_0_0_0_0_0") print $0}' > atac_20cell.sig.10.txt
+
+
+time python /Users/gzx103/Documents/2018_spring/index_caller/bin/index_caller_mvn_bg.py
+
+
+time Rscript /Users/gzx103/Documents/2018_spring/index_caller/bin/plot_count_curve.R change_num_all.10.txt change_num_all.10.counts.png 2
+
+
+time Rscript /Users/gzx103/Documents/2018_spring/index_caller/bin/plot_pheatmap_index_caller.R qda_atac_wg.mvn_check.10.txt qda_atac_wg.mvn_check.10.png ct_list.10.txt 2 red white F 0.0
+
+
+time python /Users/gzx103/Documents/2018_spring/index_caller/bin/index_caller_mvn_od_mean_mat.py
+
+
+time Rscript /Users/gzx103/Documents/2018_spring/index_caller/bin/plot_pheatmap_index_caller.R qda_atac_wg.mvn_check.10.od.txt qda_atac_wg.mvn_check.10.od.png ct_list.10.txt 2 red white F 0.0
+
+
